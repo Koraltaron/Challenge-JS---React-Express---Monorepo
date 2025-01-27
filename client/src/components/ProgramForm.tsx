bcdd65b1be2408232f9c90194bafbf1b89a6f605
Clément Praud <clement.praud6@gmail.com>
2025-01-27 10:02:51 +0100

    Add labels and style to ProgramForm component

--- client/src/components/ProgramForm.tsx
+++ client/src/components/ProgramForm.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import "./ProgramForm.css";
 
 export type ProgramData = {
   title: string;
@@ -18,6 +19,7 @@ interface ProgramFormProps {
 function ProgramForm({ children, defaultValue, onSubmit }: ProgramFormProps) {
   return (
     <form
+      className="program-form"
       onSubmit={(event) => {
         event.preventDefault();
 
@@ -40,15 +42,47 @@ function ProgramForm({ children, defaultValue, onSubmit }: ProgramFormProps) {
         });
       }}
     >
-      <input type="text" name="title" defaultValue={defaultValue.title} />
-      <input type="text" name="synopsis" defaultValue={defaultValue.synopsis} />
-      <input type="text" name="poster" defaultValue={defaultValue.poster} />
-      <input type="text" name="country" defaultValue={defaultValue.country} />
-      <input type="text" name="year" defaultValue={defaultValue.year} />
+      <label htmlFor="title">Titre de la série</label>
+      <input
+        type="text"
+        name="title"
+        defaultValue={defaultValue.title}
+        placeholder="Titre"
+      />
+      <label htmlFor="synopsis">Description de la série</label>
+      <input
+        type="text"
+        name="synopsis"
+        defaultValue={defaultValue.synopsis}
+        placeholder="Description"
+      />
+      <label htmlFor="poster">Lien de l'image de la série</label>
+      <input
+        type="text"
+        name="poster"
+        defaultValue={defaultValue.poster}
+        placeholder="URL de l'image"
+      />
+      <label htmlFor="country">Pays de production de la série</label>
+      <input
+        type="text"
+        name="country"
+        defaultValue={defaultValue.country}
+        placeholder="Pays de production"
+      />
+      <label htmlFor="year">Année de production de la série</label>
+      <input
+        type="text"
+        name="year"
+        defaultValue={defaultValue.year}
+        placeholder="Année de production"
+      />
+      <label htmlFor="category_id">Catégorie de la série</label>
       <input
         type="text"
         name="category_id"
         defaultValue={defaultValue.category_id}
+        placeholder="Catégorie de série (1, 2, ...)"
       />
       <button type="submit">{children}</button>
     </form>
